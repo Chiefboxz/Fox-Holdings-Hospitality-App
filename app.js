2b57722f1f7d101d16bc3b847b6d6c845355c61f
Rynard Van der Mescht <rynard@webmail.co.za>
2018-09-22 15:39:55 +0200

    Create app.js

---
+++ app.js
@@ -0,0 +1,14 @@
+{
+  "name": "Fox-Holdings-Hospitality-App",
+  "description": "Review app",
+  "scripts": {
+  },
+  "env": {
+  },
+  "formation": {
+  },
+  "addons": [
+   ],
+  "buildpacks": [
+   ]
+}
