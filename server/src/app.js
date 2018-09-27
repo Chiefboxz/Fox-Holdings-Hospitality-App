6be040f369299d2d3491506e281cd60441a5feec
MrRynard <rynard@webmail.co.za>
2018-09-27 13:15:38 +0200

    Added server connection and changed Database file extencion to prevent eslint interference

--- server/src/app.js
+++ server/src/app.js
@@ -1,1 +1,17 @@
-console.log('Hello world!test edit')
+const express = require('express')
+const bodyParser = require('body-parser')
+
+const port = process.env.PORT || 3000
+
+const app = express()
+app.use(bodyParser.json())
+
+app.get('/contest', (req, res) => {
+  res.send({
+    message: 'Success: Routing is working 100%'
+  })
+})
+
+app.listen(port, () => {
+  console.log(`Server started on port ${port}`)
+})
