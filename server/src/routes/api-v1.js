011be4d5fbc99659557edb4e1c8c01960120dee8
MrRynard <rynard@webmail.co.za>
2018-09-30 14:12:04 +0200

    Added comments for JKeet to follow

--- server/src/routes/api-v1.js
+++ server/src/routes/api-v1.js
@@ -3,13 +3,16 @@ const authenticationController = require('../controllers/AuthenticationContoller
 module.exports = (express) => {
   const router = express.Router()
 
-  router.get('/', (req, res) => {
-    res.send({
-      message: 'Test'
-    })
-  })
-
+  // Authentication routes
   router.post('/login', authenticationController.login)
+  router.post('/register', authenticationController.login)
+
+  /* TODO:
+   * Add email verify route
+   * Add forgot password
+   */
+
+  // JAMES ADD YOUR ROUTES HERE - LINK THEM TO CONTROLLERS SEE AUTH Controller as example
 
   return router
 }
