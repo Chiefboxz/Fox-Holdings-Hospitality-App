2200ea44dd979428961538d36b9bd3ecb0d78633
James Keet <37327253+JKeet@users.noreply.github.com>
2018-10-11 19:48:41 +0200

    Add files via upload

---
+++ server/src/models/customer.js
@@ -0,0 +1,34 @@
+const bcrypt = require('bcrypt')
+
+module.exports = (sequelize, DataTypes) => {
+  const Customer = sequelize.define('Customer', {
+    cID: {
+      type: DataTypes.INTEGER,
+      primaryKey: true
+    },
+    cFirstname: {
+      type: DataTypes.STRING
+    },
+    cLastname: {
+      type: DataTypes.STRING
+    },
+    cNumber: {
+      type: DataTypes.INTEGER,
+      unique: true
+    },
+    cEmail: {
+      type: DataTypes.STRING,
+      unique: true
+    },
+    cPassword: {
+      type: DataTypes.STRING
+    }
+  })
+
+  Customer.prototype.comparePassword = (password) => {
+    return bcrypt.compare(password, this.cPassword)
+    // return password === this.password
+  }
+
+  return Customer
+}
