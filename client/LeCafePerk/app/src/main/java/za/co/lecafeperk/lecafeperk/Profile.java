c7c71cd572bbfdb098b64e1f06b60c7535f6af45
Sidney Berry <sid.berry4@gmail.com>
2018-10-13 16:18:52 +0200

    Code for Posting Login data
    This is used to send data to server for login.
    THIS WILL CHANGE! - As soon as I can test whether it works. or not

--- client/LeCafePerk/app/src/main/java/za/co/lecafeperk/lecafeperk/Profile.java
+++ client/LeCafePerk/app/src/main/java/za/co/lecafeperk/lecafeperk/Profile.java
@@ -2,6 +2,23 @@ package za.co.lecafeperk.lecafeperk;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+import com.android.volley.Request;
+import com.android.volley.Request.Method;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.Response.Listener;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 
 public class Profile extends AppCompatActivity {
@@ -10,5 +27,64 @@ public class Profile extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+
+
     }
+
+    public void btnLoginClicked(View v) throws JSONException {
+
+        /*Toast toast = Toast.makeText(Profile.this, "No Internet Connection", Toast.LENGTH_LONG);
+        toast.show();
+        */
+
+        EditText txtEmailInput = (EditText) findViewById(R.id.txtEmail);
+        EditText txtPasswordInput = (EditText) findViewById(R.id.txtPassword);
+        String usernameInput = txtEmailInput.getText().toString();
+        String passwordInput = txtPasswordInput.getText().toString();
+        if (usernameInput.contains("@")) {
+
+            RequestQueue MyRequestQueue = Volley.newRequestQueue(this);
+            // JSONArray data = new JSONArray();
+            JSONObject data = new JSONObject();
+            data.put("email", usernameInput);
+            data.put("password", passwordInput);
+
+
+            String url = "http://./server/src/controllers/AuthenticationController/login";
+            // StringRequest MyStringRequest = new StringRequest(Request.Method.POST, url, new Response.Listener<String>() {
+
+            JsonObjectRequest MyStringRequest = new JsonObjectRequest(Method.POST, url, data , new Response.Listener<JSONObject>() {
+
+                @Override
+                public void onResponse(JSONObject response) {
+
+                    // code to execute when server responds
+                }
+
+
+
+
+
+            }, new Response.ErrorListener() { //Create an error listener to handle errors appropriately.
+                @Override
+                public void onErrorResponse(VolleyError error) {
+                    //This code is executed if there is an error.
+                    Toast toast = Toast.makeText(Profile.this, "Error connecting to server.", Toast.LENGTH_LONG);
+                    toast.show();
+                }
+            });
+
+            MyRequestQueue.add(MyStringRequest);
+
+        }
+        else{
+            Toast toast = Toast.makeText(Profile.this, "Please enter a valid email.", Toast.LENGTH_LONG);
+            toast.show();
+        }
+
+
+
+
+    }
+
 }
