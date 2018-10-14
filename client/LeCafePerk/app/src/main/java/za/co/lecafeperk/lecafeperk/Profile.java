c4bb058482f85e46f4c9cefff01908269eeda274
Bazilby <37140574+Bazilby@users.noreply.github.com>
2018-10-14 12:33:37 +0200

    UI update
    Some UI designs

--- client/LeCafePerk/app/src/main/java/za/co/lecafeperk/lecafeperk/Profile.java
+++ client/LeCafePerk/app/src/main/java/za/co/lecafeperk/lecafeperk/Profile.java
@@ -1,7 +1,9 @@
 package za.co.lecafeperk.lecafeperk;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 
 public class Profile extends AppCompatActivity {
@@ -11,4 +13,11 @@ public class Profile extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
     }
+
+    public void launchRegister (View view){
+        Intent intent = new Intent(Profile.this, registerActivity.class);
+        startActivity(intent);
+
+    }
+
 }
---
+++ client/LeCafePerk/app/src/main/java/za/co/lecafeperk/lecafeperk/registerActivity.java
@@ -0,0 +1,17 @@
+package za.co.lecafeperk.lecafeperk;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class registerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+    }
+
+    public void launchRegister(View view) {
+    }
+}
