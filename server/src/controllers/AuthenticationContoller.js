532532a4341b22ce14c7651cb37d746a4db6d337
Sidney Berry <sid.berry4@gmail.com>
2018-10-16 12:51:29 +0200

    Server change
    Done by Rynard for testing with server.

--- server/src/controllers/AuthenticationContoller.js
+++ server/src/controllers/AuthenticationContoller.js
@@ -1,6 +1,6 @@
 const { Customer } = require('../models/init')
 
-exports.login = (req, res) => {
+/* exports.login = (req, res) => {
   // var uname = req.body.name
   var email = req.body.email
   var pass = req.body.password
@@ -25,45 +25,54 @@ exports.login = (req, res) => {
       message: `The entered email is not valid.`
     })
   }
+} */
+
+exports.login = (req, res) => {
+  // var uname = req.body.name
+  res.status(200).send({
+    message: `Successfully logged in.`
+  })
+    
+    
 }
 
-  exports.register = async (req, res) => {
-    try {
-      const Customer = await Customer.create(req.body)
-      res.status(200).send({
-        message: `The account with ${req.body.email} was created successfuly`,
-        Customer: Customer.toJSON()
-      })
-    } catch (err) {
-      res.status(400).send({
-        message: `${req.body.email} are already in use`
-      })
-    }
-    res.send({
-      message: `You have registered with ${req.body.email} succesfully`
+exports.register = async (req, res) => {
+  try {
+    const Customer = await Customer.create(req.body)
+    res.status(200).send({
+      message: `The account with ${req.body.email} was created successfuly`,
+      Customer: Customer.toJSON()
+    })
+  } catch (err) {
+    res.status(400).send({
+      message: `${req.body.email} are already in use`
     })
   }
+  res.send({
+    message: `You have registered with ${req.body.email} succesfully`
+  })
+}
 
-  exports.updateUser = async (req, res) => {
-    var firstname = req.body.fname
-    var lastname = req.body.lname
-    var number = req.body.num
-    var email = req.body.email
+exports.updateUser = async (req, res) => {
+  var firstname = req.body.fname
+  var lastname = req.body.lname
+  var number = req.body.num
+  var email = req.body.email
 
-    try {
-      Customer.update({
-        cFirstname: firstname,
-        cLastname: lastname,
-        cNumber: number
+  try {
+    Customer.update({
+      cFirstname: firstname,
+      cLastname: lastname,
+      cNumber: number
 
-      }, {
-        where: {
-          cEmail: email
-        }
-      })
-    } catch (err) {
-      res.status(400).send({
-        message: `Something went wrong!!!`
-      })
-    }
+    }, {
+      where: {
+        cEmail: email
+      }
+    })
+  } catch (err) {
+    res.status(400).send({
+      message: `Something went wrong!!!`
+    })
   }
+}
